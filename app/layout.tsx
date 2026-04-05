a635517d73f23673f85a4d43a26a0f4663b44720
haahmed571 <haahmed571@gmail.com>
2026-04-05 11:15:28 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Hassan Ahmed",
+  description: "AWS Network Deployment Technician Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
